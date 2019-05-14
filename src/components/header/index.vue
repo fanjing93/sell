5d2b1578c1f63c9406422e1c2a42503516ce803e
fanjing <fanjing@wemart.cn>
2019-05-14 12:20:42 +0800

    update

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -10,8 +10,8 @@
           <span class="name">{{ seller['name'] }}</span>
         </div>
         <div class="description">{{ seller['description']}}/{{seller['deliveryTime'] }}分钟送达</div>
-        <div class="support" v-if="seller['supports'].length">
-          <span class="icon"></span>
+        <div class="support" v-if="seller['supports']">
+          <span class="icon" :class="iconClass"></span>
           <span class="text">{{ seller['supports'][0]['description'] }}</span>
         </div>
       </div>
@@ -23,6 +23,11 @@
 <script>
   export default {
     name: 'index',
+    data () {
+      return {
+        classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+      }
+    },
     props: {
       seller: {
         type: Object,
@@ -31,8 +36,10 @@
         }
       }
     },
-    data () {
-      return {}
+    computed: {
+      iconClass () {
+        return this.classMap[this.seller['supports'][0]['type']]
+      }
     }
   }
 </script>
@@ -43,10 +50,11 @@
   .header
     color: #FFFFFF
     background-color: #000000
+    display: flex
 
     .content-wrapper
       display: flex
-      align-items: flex-end
+      align-items: flex-start
       padding: 24px 12px 18px 24px
 
       .content
@@ -55,8 +63,8 @@
 
         .title
           display: flex
-          align-items: center
-          margin: 2px 0 8px 0
+          align-items: flex-start
+          margin: 2px 0 8px
 
           .brand
             display: inline-block
@@ -67,4 +75,43 @@
             background-size: 30px 18px
             background-repeat: no-repeat
 
+          .name
+            margin-left: 6px
+            font-size: 16px
+            font-weight: bold
+
+        .description
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: 12px
+
+        .support
+          display: flex
+          align-items flex-start
+
+          .icon
+            display: inline-block
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            -webkit-background-size: 12px 12px
+            background-size: 12px 12px
+
+            &.decrease
+              bg-img('decrease_1')
+
+            &.discount
+              bg-img('discount_1')
+
+            &.guarantee
+              bg-img('guarantee_1')
+
+            &.invoice
+              bg-img('invoice_1')
+
+            &.special
+              bg-img('special_1')
+
+          .text
+            font-size: 12px
 </style>
